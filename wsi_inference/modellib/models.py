d9f0f30496210102e5b34f9355bd6fa96e0c5e5e
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-08-26 16:12:51 -0400

    enable loading model weights from internet
    This commit adds a Weights object which stores information about a
    trained model. This includes the url to weights, number of output
    classes, patch size, patch spacing, and metadata.
    
    In the future, we might want to add the human-readable classes list to
    the Weights object, because it makes sense for them to be kept with the
    weights.

--- wsi_inference/modellib/models.py
+++ wsi_inference/modellib/models.py
@@ -2,65 +2,141 @@
 
 See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7369575/table/tbl3/ for modifications
 that were made to the original architectures.
-
-All model functions must have the signature:
-    Function[[int, Optional[str]], torch.nn.Module]
 """
 
+import dataclasses
 import pathlib
-import typing
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Union
 
+from PIL import Image
 import torch
 from torch.hub import load_state_dict_from_url
 import torchvision
 
 from .inceptionv4 import inceptionv4 as _inceptionv4
-from .inceptionv4 import InceptionV4 as _InceptionV4
+from .transforms import PatchClassification
 
-# TODO: consider adding the color normalization and input size here. Or maybe as a
-# command line argument.
-# One way forward is to follow in Torchvision's footsteps and create a Weights dataclass
-# to hold the url, transform function, and metadata. That is easy enough, but then how
-# do we associate each model with its available weights? Torchvision uses subclassed
-# enums but for this project, let's go with something simpler.
-
-PathType = typing.Union[str, pathlib.Path]
+PathType = Union[str, pathlib.Path]
 
 
-def inceptionv4(num_classes: int, weights="TCGA-BRCA-v1") -> _InceptionV4:
-    """Create InceptionV4 model."""
-    model = _inceptionv4(num_classes=num_classes, pretrained=False)
-    if weights == "TCGA-BRCA-v1":
-        print("Loading state dict")
-        state_dict = load_state_dict_from_url(
+class ModelNotFoundError(Exception):
+    ...
+
+
+class WeightsNotFoundError(Exception):
+    ...
+
+
+@dataclasses.dataclass
+class Weights:
+    """Container for weights URL and associated information."""
+
+    url: str
+    file_name: str
+    num_classes: int
+    transform: Callable[[Union[Image.Image, torch.Tensor]], torch.Tensor]
+    patch_size_pixels: int
+    spacing_um_px: float
+    metadata: Dict[str, Any]
+
+
+# Store all available weights for all models.
+WEIGHTS: Dict[str, Dict[str, Weights]] = {
+    "inceptionv4": {
+        "TCGA-BRCA-v1": Weights(
             url="https://stonybrookmedicine.box.com/shared/static/tfwimlf3ygyga1x4fnn03u9y5uio8gqk.pt",  # noqa
-            check_hash=True,
             file_name="inceptionv4-brca-20190613-aef40942.pt",
-        )
-        model.load_state_dict(state_dict, strict=True)
-    else:
-        raise NotImplementedError("only TCGA-BRCA-v1 is available now.")
-    return model
-
-
-def resnet34(num_classes: int, weights="TCGA-BRCA-v1") -> torchvision.models.ResNet:
-    """Create ResNet34 model."""
-    model = torchvision.models.resnet34()
-    model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
-    if weights == "TCGA-BRCA-v1":
-        print("Loading state dict")
-        state_dict = load_state_dict_from_url(
+            num_classes=2,
+            transform=PatchClassification(
+                resize_size=299,
+                # TODO: check that the mean and std dev are correct.
+                mean=(0.5, 0.5, 0.5),
+                std=(0.5, 0.5, 0.5),
+            ),
+            patch_size_pixels=350,
+            spacing_um_px=88 / 350,
+            metadata={"patch-size": "350 pixels (88 microns)."},
+        ),
+    },
+    "resnet34": {
+        "TCGA-BRCA-v1": Weights(
             url="https://stonybrookmedicine.box.com/shared/static/dv5bxk6d15uhmcegs9lz6q70yrmwx96p.pt",  # noqa
-            check_hash=True,
             file_name="resnet34-brca-20190613-01eaf604.pt",
+            num_classes=2,
+            transform=PatchClassification(
+                resize_size=224,
+                mean=(0.7238, 0.5716, 0.6779),
+                std=(0.1120, 0.1459, 0.1089),
+            ),
+            patch_size_pixels=350,
+            spacing_um_px=88 / 350,
+            metadata={"patch-size": "350 pixels (88 microns)."},
         )
-        model.load_state_dict(state_dict, strict=True)
-    else:
-        raise NotImplementedError("only TCGA-BRCA-v1 is available now.")
+    },
+    "vgg16_modified": {
+        "TCGA-BRCA-v1": Weights(
+            url="https://stonybrookmedicine.box.com/shared/static/197s56yvcrdpan7eu5tq8d4gxvq3xded.pt",  # noqa
+            file_name="vgg16-modified-brca-20190613-62bc1b41.pt",
+            num_classes=2,
+            transform=PatchClassification(
+                resize_size=224,
+                mean=(0.7238, 0.5716, 0.6779),
+                std=(0.1120, 0.1459, 0.1089),
+            ),
+            patch_size_pixels=350,
+            spacing_um_px=88 / 350,
+            metadata={"patch-size": "350 pixels (88 microns)."},
+        )
+    },
+}
+
+
+def _get_model_weights(model_name: str, weights: str) -> Weights:
+    all_weights_for_model = WEIGHTS.get(model_name)
+    if all_weights_for_model is None:
+        raise ModelNotFoundError(f"no weights registered for {model_name}")
+    weights_obj = all_weights_for_model.get(weights)
+    if weights_obj is None:
+        raise WeightsNotFoundError(f"'{weights}' weights not found for {model_name}")
+    return weights_obj
+
+
+def _load_state_into_model(model: torch.nn.Module, weights: Weights):
+    print("Information about the pretrained weights")
+    print("----------------------------------------")
+    for k, v in dataclasses.asdict(weights).items():
+        print(f"{k} = {v}")
+    print("----------------------------------------\n")
+    state_dict = load_state_dict_from_url(
+        url=weights.url, check_hash=True, file_name=weights.file_name
+    )
+    model.load_state_dict(state_dict, strict=True)
     return model
 
 
-def vgg16_modified(num_classes: int, weights="TCGA-BRCA-v1") -> torch.nn.Module:
+def inceptionv4(weights: str = "TCGA-BRCA-v1") -> Tuple[torch.nn.Module, Weights]:
+    """Create InceptionV4 model."""
+    weights_obj = _get_model_weights("inceptionv4", weights=weights)
+    model = _inceptionv4(num_classes=weights_obj.num_classes)
+    model = _load_state_into_model(model=model, weights=weights_obj)
+    return model, weights_obj
+
+
+def resnet34(weights: str = "TCGA-BRCA-v1") -> Tuple[torch.nn.Module, Weights]:
+    """Create ResNet34 model."""
+    weights_obj = _get_model_weights("resnet34", weights=weights)
+    model = torchvision.models.resnet34()
+    model.fc = torch.nn.Linear(model.fc.in_features, weights_obj.num_classes)
+    model = _load_state_into_model(model=model, weights=weights_obj)
+    return model, weights_obj
+
+
+def vgg16_modified(weights="TCGA-BRCA-v1") -> Tuple[torch.nn.Module, Weights]:
     """Create modified VGG16 model.
 
     The classifier of this model is
@@ -68,48 +144,36 @@ def vgg16_modified(num_classes: int, weights="TCGA-BRCA-v1") -> torch.nn.Module:
         ReLU -> Dropout
         Linear (1024, num_classes)
     """
+    weights_obj = _get_model_weights("vgg16_modified", weights=weights)
     model = torchvision.models.vgg16()
     model.classifier = model.classifier[:4]
     in_features = model.classifier[0].in_features
     model.classifier[0] = torch.nn.Linear(in_features, 1024)
-    model.classifier[3] = torch.nn.Linear(1024, num_classes)
-    if weights == "TCGA-BRCA-v1":
-        print("Loading state dict")
-        state_dict = load_state_dict_from_url(
-            url="https://stonybrookmedicine.box.com/shared/static/197s56yvcrdpan7eu5tq8d4gxvq3xded.pt",  # noqa
-            check_hash=True,
-            file_name="vgg16-modified-brca-20190613-62bc1b41.pt",
-        )
-        model.load_state_dict(state_dict, strict=True)
-    else:
-        raise NotImplementedError("only TCGA-BRCA-v1 is available now.")
-    return model
+    model.classifier[3] = torch.nn.Linear(1024, weights_obj.num_classes)
+    model = _load_state_into_model(model=model, weights=weights_obj)
+    return model, weights_obj
 
 
-MODELS: typing.Dict[str, typing.Callable[..., torch.nn.Module]] = dict(
+MODELS = dict(
     inceptionv4=inceptionv4,
     resnet34=resnet34,
     vgg16_modified=vgg16_modified,
 )
 
 
-class ModelNotFoundError(Exception):
-    ...
-
-
-def list_models() -> typing.List[str]:
+def list_models() -> List[str]:
     return list(MODELS.keys())
 
 
 def create_model(
     model_name: str,
-    num_classes: int,
     weights: str = "TCGA-BRCA-v1",
-) -> torch.nn.Module:
+) -> Tuple[torch.nn.Module, Weights]:
     """Create a model."""
     if model_name not in MODELS.keys():
         raise ModelNotFoundError(
             f"{model_name} not found. Available models are {MODELS.keys()}"
         )
     model_fn = MODELS[model_name]
-    return model_fn(num_classes=num_classes, weights=weights)
+    model, weights_obj = model_fn(weights=weights)
+    return model, weights_obj
