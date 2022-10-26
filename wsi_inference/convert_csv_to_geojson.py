9ef9c143a189bd2415bc006d7acc5d00c7a7eff0
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-10-26 16:37:16 -0400

    accept a directory of CSVs and convert all

--- wsi_inference/convert_csv_to_geojson.py
+++ wsi_inference/convert_csv_to_geojson.py
@@ -1,6 +1,6 @@
-"""Convert CSV of model outputs to a GeoJSON file.
+"""Convert CSVs of model outputs to GeoJSON files.
 
-This GeoJSON file can be loaded into whole slide image viewers like QuPath.
+GeoJSON files can be loaded into whole slide image viewers like QuPath.
 """
 
 import json
@@ -9,6 +9,7 @@ import typing
 
 import click
 import pandas as pd
+import tqdm
 
 
 def _box_to_polygon(
@@ -74,21 +75,52 @@ def _version() -> str:
 
 
 @click.command()
-@click.argument("input", type=click.Path(exists=True, dir_okay=False, path_type=Path))
+@click.argument(
+    "results_dir",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, path_type=Path, resolve_path=True
+    ),
+)
 @click.argument(
     "output",
-    type=click.Path(dir_okay=False, exists=False, writable=True, path_type=Path),
+    type=click.Path(exists=False, path_type=Path, resolve_path=True),
 )
 @click.version_option(version=_version())
-def cli(*, input: Path, output: Path):
-    """Convert CSV of patch predictions to a GeoJSON file.
+def cli(*, results_dir: Path, output: Path):
+    """Convert CSVs of patch predictions to GeoJSON files.
 
     GeoJSON files can be used with pathology viewers like QuPath.
 
-    INPUT       Path to input CSV
+    RESULTS_DIR     Path to results directory (containing model-outputs dir).
 
-    OUTPUT      Path to output GeoJSON file (with .json extension)
+    OUTPUT          Path to output directory in which to save GeoJSON files.
     """
-    click.echo(f"Reading CSV: {input}")
-    convert(input=input, output=output)
-    click.secho(f"Saved output to {output}", fg="green")
+    if not results_dir.exists():
+        raise click.ClickException(f"results_dir does not exist: {results_dir}")
+    if output.exists():
+        raise click.ClickException("Output directory already exists.")
+    if (
+        not (results_dir / "model-outputs").exists()
+        and (results_dir / "patches").exists()
+    ):
+        raise click.ClickException(
+            "Model outputs have not been generated yet. Please run model inference."
+        )
+    if not (results_dir / "model-outputs").exists():
+        raise click.ClickException(
+            "Expected results_dir to contain a 'model-outputs' directory but it does"
+            " not. Please provide the path to the directory that contains"
+            " model-outputs, masks, and patches."
+        )
+
+    csvs = list((results_dir / "model-outputs").glob("*.csv"))
+    if not csvs:
+        raise click.ClickException("No CSVs found. Did you generate model outputs?")
+
+    output.mkdir(exist_ok=False)
+
+    for input_csv in tqdm.tqdm(csvs):
+        output_path = output / input_csv.with_suffix(".json").name
+        convert(input=input_csv, output=output_path)
+
+    click.secho(f"Saved outputs to {output}", fg="green")
