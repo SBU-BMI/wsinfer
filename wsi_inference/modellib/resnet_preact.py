a3a0fc13fdd54e7ee60b882d06884d96bea2fb63
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-10-26 21:36:13 -0400

    add kernel=7 to conv1 and maxpool layer

--- wsi_inference/modellib/resnet_preact.py
+++ wsi_inference/modellib/resnet_preact.py
@@ -10,7 +10,7 @@ class PreActBlock(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
-        super(PreActBlock, self).__init__()
+        super().__init__()
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
@@ -80,11 +80,13 @@ class PreActBottleneck(nn.Module):
 
 
 class PreActResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=1):
         super(PreActResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=1, bias=False)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -101,13 +103,15 @@ class PreActResNet(nn.Module):
 
     def forward(self, x):
         out = self.conv1(x)
+        out = self.maxpool(out)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, out.size(2))
         out = out.view(out.size(0), -1)
         out = self.linear(out)
+        out = out.squeeze()
         return out
 
 
