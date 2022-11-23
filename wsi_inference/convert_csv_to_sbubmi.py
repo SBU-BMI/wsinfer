738606039c3caef26af3088812f7eeb8b4b41eec
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-11-23 13:19:01 -0500

    fix prediction- headers + print info + fix txt save paths

--- wsi_inference/convert_csv_to_sbubmi.py
+++ wsi_inference/convert_csv_to_sbubmi.py
@@ -174,10 +174,11 @@ def write_heatmap_txt(input: PathType, output: PathType, class_names: typing.Lis
     # TODO: should we round and cast to int here?
     df.loc[:, "x_loc"] = (df.minx + (df.width / 2)).round().astype(int)
     df.loc[:, "y_loc"] = (df.miny + (df.height / 2)).round().astype(int)
-    cols = [f"prob_{c}" for c in class_names]
-    cols = ["x_loc", "y_loc", *cols]
-    df = df.loc[:, cols]  # Rename 'prob_NAME' columns to 'NAME'.
-    df = df.rename(columns={c: c[5:] for c in cols})
+    prob_cols = [f"prob_{c}" for c in class_names]
+    cols = ["x_loc", "y_loc", *prob_cols]
+    df = df.loc[:, cols]
+    # Rename 'prob_NAME' columns to 'NAME'.
+    df = df.rename(columns={c: c[5:] for c in prob_cols})
     df.to_csv(output, index=False, sep=" ")
 
 
@@ -220,10 +221,6 @@ def write_color_txt(
 
     df = pd.read_csv(input)
     df_rows_as_dicts = df.to_dict("records")
-
-    print("Calculating color information for each patch in the whole slide image...")
-    print("This might take some time.")
-    print("Use a large --num-processes value to hopefully speed it up.")
 
     # https://stackoverflow.com/a/45276885/5666087
     with multiprocessing.Pool(num_processes) as p:
@@ -357,7 +354,9 @@ def cli(
         print(f"  - {c}")
     print("-" * 40)
 
-    for input_csv in tqdm.tqdm(csvs):
+    for ii, input_csv in enumerate(csvs):
+        print("-" * 40)
+        print(f"Converting outputs for slide {ii+1} of {len(csvs)}")
         slide_id = input_csv.stem
         wsi_file = wsi_dir / f"{slide_id}.svs"
         if not wsi_file.exists():
@@ -382,7 +381,10 @@ def cli(
             output_meta = output_heatmap.parent / f"meta_{slide_id}.json"
             output_heatmap.parent.mkdir(parents=True, exist_ok=True)
 
-            click.echo(f"Writing JSON lines file: {output_heatmap}")
+            click.echo("Writing JSON heatmap:")
+            click.echo(f"  {output_heatmap}")
+            click.echo("Writing JSON meta:")
+            click.echo(f"  {output_meta}")
             write_heatmap_and_meta_json_lines(
                 input=input_csv,
                 output_heatmap=output_heatmap,
@@ -397,9 +399,18 @@ def cli(
                 run_metadata=run_metadata,
             )
 
-            output_txt_prediction = (
-                output / "heatmap_txt" / class_name / f"prediction-{slide_id}"
-            )
+            # Do not use class-specific dirs if there is only one class.
+            if len(class_names) == 1:
+                output_txt_prediction = (
+                    output / "heatmap_txt" / f"prediction-{slide_id}"
+                )
+            else:
+                output_txt_prediction = (
+                    output / "heatmap_txt" / class_name / f"prediction-{slide_id}"
+                )
+            output_txt_prediction.parent.mkdir(parents=True, exist_ok=True)
+            print("Writing TXT heatmap:")
+            print(f"  {output_txt_prediction}")
             write_heatmap_txt(
                 input=input_csv, output=output_txt_prediction, class_names=[class_name]
             )
@@ -407,8 +418,10 @@ def cli(
         # Do this once per WSI. If the model has multi-class outputs, copy the color
         # file into each class-specific directory.
         if make_color_text:
+            print("Writing color TXT file with color information for each patch")
             if len(class_names) == 1:
                 output_color = output / "heatmap_txt" / f"color-{slide_id}"
+                print(f"  {output_color}")
                 write_color_txt(
                     input=input_csv,
                     output=output_color,
@@ -419,6 +432,7 @@ def cli(
                 output_color = (
                     output / "heatmap_txt" / class_names[0] / f"color-{slide_id}"
                 )
+                print(f"  {output_color}")
                 write_color_txt(
                     input=input_csv,
                     output=output_color,
@@ -426,10 +440,10 @@ def cli(
                     num_processes=num_processes,
                 )
                 # Copy this color file to all class-specific dirs.
+                print("Copying color TXT file to class-specific TXT directories")
                 for class_name in class_names[1:]:
-                    shutil.copy(
-                        output_color,
-                        output_color.parent.parent / class_name / output_color.name,
-                    )
+                    cp_dst = output_color.parent.parent / class_name / output_color.name
+                    print(f"  {cp_dst}")
+                    shutil.copy(output_color, cp_dst)
 
     click.secho("Finished.", fg="green")
