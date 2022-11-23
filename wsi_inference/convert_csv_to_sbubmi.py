98c03e0fb0fb929bebd8fd08de6d680636fd3f4f
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-11-22 20:07:37 -0500

    apply to dir of outputs + fix json heatmaps

--- wsi_inference/convert_csv_to_sbubmi.py
+++ wsi_inference/convert_csv_to_sbubmi.py
@@ -1,12 +1,12 @@
 """Convert CSV of model outputs to Stony Brook BMI formats.
 
-Directories:
-heatmap_jsons (same as json)
-heatmap_txt
-json (same as heatmap_jsons)
-patch-level-color
-patch-level-lym
-patch-level-merged
+Output directory tree:
+├── heatmap_jsons
+│   ├── heatmap-SLIDEID.json
+│   └── meta-SLIDEID.json
+└── heatmap_txt
+    ├── color-SLIDEID
+    └── prediction-SLIDEID
 """
 
 from datetime import datetime
@@ -42,14 +42,16 @@ def _get_timestamp() -> str:
     return dt.strftime("%Y-%m-%d_%H:%M:%S %Z")
 
 
-def _row_to_heatmap_row(
+def _row_to_heatmap_json_row(
     row: pd.Series,
     *,
     class_name: str,
     slide_width: int,
     slide_height: int,
-    case_id: typing.Optional[str] = None,
-    subject_id: typing.Optional[str] = None,
+    execution_id: str,
+    study_id: str,
+    case_id: str,
+    subject_id: str,
 ) -> typing.Dict:
     minx, miny, width, height = row["minx"], row["miny"], row["width"], row["height"]
     patch_area_base_pixels = width * height
@@ -84,9 +86,9 @@ def _row_to_heatmap_row(
         "provenance": {
             "analysis": {
                 "source": "computer",
-                "execution_id": "tcga-brca-jakub-refactor-pipeline",
+                "execution_id": execution_id,
                 "cancer_type": "quip",
-                "study_id": "TCGA-BRCA",
+                "study_id": study_id,
                 "computation": "heatmap",
             },
             "image": {
@@ -98,9 +100,9 @@ def _row_to_heatmap_row(
         "properties": {
             "multiheat_param": {
                 "human_weight": -1,
-                "metric_array": [class_probability, 0.0],
-                "heatname_array": ["tumor", "necrosis"],
-                "weight_array": ["0.5", "0.5"],
+                "metric_array": [class_probability],
+                "heatname_array": ["tumor"],  # TODO: change the name.
+                "weight_array": ["1"],
             },
             "metric_value": class_probability,
             "metric_type": "tile_dice",
@@ -110,29 +112,31 @@ def _row_to_heatmap_row(
     }
 
 
-def write_heatmap_json_like(
+def write_heatmap_json_lines(
     input: PathType,
     output: PathType,
-    class_name: str,
-    slide_width: int,
-    slide_height: int,
-    case_id: typing.Optional[str] = None,
-    subject_id: typing.Optional[str] = None,
+    slide_width: PathType,
+    slide_height: PathType,
+    execution_id: str,
+    study_id: str,
+    case_id: str,
+    subject_id: str,
 ) -> None:
     df = pd.read_csv(input)
     features = df.apply(
-        _row_to_heatmap_row,
+        _row_to_heatmap_json_row,
         axis=1,
-        class_name=class_name,
         slide_width=slide_width,
         slide_height=slide_height,
+        execution_id=execution_id,
+        study_id=study_id,
         case_id=case_id,
         subject_id=subject_id,
     )
     features = features.tolist()
-    features_json = (json.dumps(row) for row in features)
+    features = (json.dumps(row) for row in features)
     with open(output, "w") as f:
-        f.writelines(line + "\n" for line in features_json)
+        f.writelines(line + "\n" for line in features)
 
 
 def write_heatmap_txt(input: PathType, output: PathType, class_name: str):
@@ -225,81 +229,124 @@ def _version() -> str:
 
 
 @click.command()
-@click.argument("input", type=click.Path(exists=True, dir_okay=False, path_type=Path))
-@click.option(
-    "--output-dir",
-    type=click.Path(path_type=Path),
-    help="Directory in which to save outputs.",
+@click.argument(
+    "results_dir",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, path_type=Path, resolve_path=True
+    ),
 )
-# TODO: how are multi-class predictions saved?
+@click.argument(
+    "output",
+    type=click.Path(exists=False, path_type=Path, resolve_path=True),
+)
 @click.option(
-    "--class-name",
+    "--wsi-dir",
     required=True,
-    help="Name of the class to use for probability values.",
+    help="Directory with whole slide images.",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, path_type=Path, resolve_path=True
+    ),
 )
+@click.option("--execution-id", required=True, help="Unique execution ID for this run.")
+@click.option("--study-id", required=True, help="Study ID, like TCGA-BRCA.")
 @click.option(
-    "--slide",
-    type=click.Path(exists=True, path_type=Path),
-    required=True,
-    help="Path to original whole slide image (mutually exclusive with slide_width"
-    " and slide_height)",
+    "--make-color-text/--no-make-color-text",
+    default=False,
+    help="Make text files with color information for each patch. NOTE: this can add"
+    " several minutes of processing time per slide.",
 )
-@click.option("--subject-id", default=None, help="Subject ID")
-@click.option("--case-id", default=None, help="Subject's case ID")
 @click.option(
     "--num-processes",
     type=int,
     default=4,
-    help="Number of processes to use when creating color text file.",
+    help="Number of processes to use when `--make-color-text` is enabled.",
 )
 @click.version_option(version=_version())
 def cli(
     *,
-    input: Path,
-    output_dir: Path,
-    class_name: str,
-    slide: Path,
-    subject_id: typing.Optional[str] = None,
-    case_id: typing.Optional[str] = None,
+    results_dir: Path,
+    output: Path,
+    wsi_dir: Path,
+    execution_id: str,
+    study_id: str,
+    make_color_text: bool = False,
     num_processes: int = 4,
 ):
     """Convert CSV of patch predictions to .txt and .json formats for use with Stony
     Brook Biomedical Informatics viewers.
 
-    INPUT           Path to input CSV
+    RESULTS_DIR     Path to results directory (containing model-outputs dir).
+
+    OUTPUT          Path to output directory in which to save files.
     """
-    click.echo(f"Reading CSV: {input}")
-    ts: large_image.tilesource.TileSource = large_image.getTileSource(slide)
-    slide_width: int = ts.getMetadata()["sizeX"]
-    slide_height: int = ts.getMetadata()["sizeY"]
-    print(f"Slide dimensions: {slide_width} x {slide_height} (width x height)")
+    if not results_dir.exists():
+        raise click.ClickException(f"results_dir does not exist: {results_dir}")
+    if output.exists():
+        raise click.ClickException("Output directory already exists.")
+    if (
+        not (results_dir / "model-outputs").exists()
+        and (results_dir / "patches").exists()
+    ):
+        raise click.ClickException(
+            "Model outputs have not been generated yet. Please run model inference."
+        )
+    if not (results_dir / "model-outputs").exists():
+        raise click.ClickException(
+            "Expected results_dir to contain a 'model-outputs' directory but it does"
+            " not. Please provide the path to the directory that contains"
+            " model-outputs, masks, and patches."
+        )
+    if not wsi_dir.exists():
+        raise click.ClickException("Whole slide image directory does not exist.")
 
-    slide_id = slide.stem
+    csvs = list((results_dir / "model-outputs").glob("*.csv"))
+    if not csvs:
+        raise click.ClickException("No CSVs found. Did you generate model outputs?")
 
-    if not output_dir.exists():
-        print(f"Making directory {output_dir}")
-        output_dir.mkdir()
+    output.mkdir(exist_ok=False)
 
-    # Set up directories.
-    (output_dir / "json").mkdir(exist_ok=True)
-    (output_dir / "heatmap_jsons").mkdir(exist_ok=True)
-    (output_dir / "heatmap_txt").mkdir(exist_ok=True)
-    (output_dir / "patch-level-color").mkdir(exist_ok=True)
-    (output_dir / "patch-level-merged").mkdir(exist_ok=True)
+    # Get the output classes of the model. We will create separate directories for each
+    # label name. But by default, we do not include labels that start with "no", like
+    # "notils" and "notumor".
+    class_names: typing.Sequence[str] = pd.read_csv(csvs[0]).columns
+    class_names = [n for n in class_names if n.startswith("prob_")]
+    ignore_names = {"notils", "notumor"}
+    class_names = [n for n in class_names if n not in ignore_names]
 
-    # Write JSON files with heatmap info.
-    jsonl_output = output_dir / "json" / f"heatmap_{slide_id}.json"
-    write_heatmap_json_like(
-        input=input,
-        output=jsonl_output,
-        class_name=class_name,
-        slide_width=slide_width,
-        slide_height=slide_height,
-        case_id=case_id,
-        subject_id=subject_id,
-    )
-    shutil.copy(jsonl_output, output_dir / "heatmap_jsons")
-    click.secho("Saved JSON heatmaps.", fg="green")
+    for input_csv in tqdm.tqdm(csvs):
+        slide_id = input_csv.stem
+        wsi_file = wsi_dir / f"{slide_id}.svs"
+        if not wsi_file.exists():
+            click.secho(f"WSI file not found: {wsi_file}", bg="red")
+            click.secho("Skipping...", bg="red")
+            continue
+        ts = large_image.getTileSource(wsi_file)
+        if ts.sizeX is None or ts.sizeY is None:
+            click.secho(f"Unknown size for WSI: {wsi_file}", bg="red")
+            click.secho("Skipping...", bg="red")
+            continue
+
+        for class_name in class_names:
+            output_path = (
+                output / "heatmap_json" / class_name / f"heatmap_{slide_id}.json"
+            )
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+            click.echo(f"Writing JSON file: {output_path}")
+            write_heatmap_json_lines(
+                input=input_csv,
+                output=output_path,
+                slide_width=ts.sizeX,
+                slide_height=ts.sizeY,
+                execution_id=execution_id,
+                study_id=study_id,
+                case_id=slide_id,  # TODO: should case_id be different?
+                subject_id=slide_id,
+            )
+
+            # TODO: write meta json files.
+            # TODO: write text files.
+
+    return
 
     # Write tables with predictions.
     table_output = output_dir / "heatmap_txt" / f"prediction-{slide_id}"
@@ -310,11 +357,12 @@ def cli(
     # TODO: add patch-level-CLASS files.
 
     # Write color tables.
-    color_output = output_dir / "heatmap_txt" / f"prediction-{slide_id}"
-    write_color_txt(
-        input=input, output=color_output, ts=ts, num_processes=num_processes
-    )
-    shutil.copy(color_output, output_dir / "patch-level-color")
-    click.secho("Saved table with color info.", fg="green")
+    if make_color_text:
+        color_output = output_dir / "heatmap_txt" / f"prediction-{slide_id}"
+        write_color_txt(
+            input=input, output=color_output, ts=ts, num_processes=num_processes
+        )
+        shutil.copy(color_output, output_dir / "patch-level-color")
+        click.secho("Saved table with color info.", fg="green")
 
     click.secho("Finished.", fg="green")
