01b961a13afab5ae4627e5e529791261077a3c2f
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-12-20 15:37:54 -0500

    add "wsinfer list" cli

--- wsinfer/cli/cli.py
+++ wsinfer/cli/cli.py
@@ -4,7 +4,9 @@ import click
 from .convert_csv_to_geojson import cli as _cli_convert_to_geojson
 from .convert_csv_to_sbubmi import cli as _cli_convert_to_sbubmi
 from .infer import cli as _cli_inference
+from .list_models_and_weights import cli as _cli_list_models_weights
 from ..patchlib.create_patches_fp import cli as _cli_patch
+
 
 # We use invoke_without_command=True so that 'wsinfer' on its own can be used for
 # inference on slides.
@@ -19,3 +21,4 @@ cli.add_command(_cli_inference, name="run")
 cli.add_command(_cli_convert_to_geojson, name="togeojson")
 cli.add_command(_cli_convert_to_sbubmi, name="tosbu")
 cli.add_command(_cli_patch, name="patch")
+cli.add_command(_cli_list_models_weights, name="list")
