4baaa4bf01c0fb82ccbd76263a3e4a44d0b62e5a
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-12-24 10:30:40 -0500

    add tests with config

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 import sys
 
+import click
 from click.testing import CliRunner
 import numpy as np
 import pandas as pd
@@ -50,7 +51,48 @@ def test_cli_list():
     assert result.exit_code == 0
 
 
+def test_cli_run_args(tmp_path: Path):
+    """Test that (model and weights) or config is required."""
+    from wsinfer.cli.cli import cli
+
+    wsi_dir = tmp_path / "slides"
+    wsi_dir.mkdir()
+
+    runner = CliRunner()
+    args = [
+        "run",
+        "--wsi-dir",
+        wsi_dir,
+        "--results-dir",
+        tmp_path / "results",
+    ]
+    # No model, weights, or config.
+    result = runner.invoke(cli, args)
+    assert result.exit_code != 0
+    assert "one of (model and weights) or config is required." in result.output
+
+    # Only one of model and weights.
+    result = runner.invoke(cli, [*args, "--model", "resnet34"])
+    assert result.exit_code != 0
+    assert "model and weights must both be set if one is set." in result.output
+    result = runner.invoke(cli, [*args, "--weights", "TCGA-BRCA-v1"])
+    assert result.exit_code != 0
+    assert "model and weights must both be set if one is set." in result.output
+
+    # config and model
+    result = runner.invoke(cli, [*args, "--config", __file__, "--model", "resnet34"])
+    assert result.exit_code != 0
+    assert "model and weights are mutually exclusive with config." in result.output
+    # config and weights
+    result = runner.invoke(
+        cli, [*args, "--config", __file__, "--weights", "TCGA-BRCA-v1"]
+    )
+    assert result.exit_code != 0
+    assert "model and weights are mutually exclusive with config." in result.output
+
+
 def test_cli_run_and_convert(tiff_image: Path, tmp_path: Path):
+    """This is a form of a regression test."""
     from wsinfer.cli.cli import cli
 
     runner = CliRunner()
@@ -139,3 +181,46 @@ def test_cli_run_and_convert(tiff_image: Path, tmp_path: Path):
 def test_convert_to_sbu():
     # TODO: create a synthetic output and then convert it. Check that it is valid.
     assert False
+
+
+def test_cli_run_from_config(tiff_image: Path, tmp_path: Path):
+    """This is a form of a regression test."""
+    import wsinfer
+    from wsinfer.cli.cli import cli
+
+    # Use config for resnet34 TCGA-BRCA-v1 weights.
+    config = Path(wsinfer.__file__).parent / "modeldefs" / "resnet34_tcga-brca-v1.yaml"
+    assert config.exists()
+
+    runner = CliRunner()
+    results_dir = tmp_path / "inference"
+    result = runner.invoke(
+        cli,
+        [
+            "run",
+            "--wsi-dir",
+            tiff_image.parent,
+            "--config",
+            config,
+            "--results-dir",
+            results_dir,
+        ],
+    )
+    assert result.exit_code == 0
+    assert (results_dir / "model-outputs").exists()
+    df = pd.read_csv(results_dir / "model-outputs" / "purple.csv")
+    assert df.columns.tolist() == [
+        "slide",
+        "minx",
+        "miny",
+        "width",
+        "height",
+        "prob_notumor",
+        "prob_tumor",
+    ]
+    assert (df.loc[:, "slide"] == str(tiff_image)).all()
+    assert (df.loc[:, "width"] == 350).all()
+    assert (df.loc[:, "height"] == 350).all()
+    assert (df.loc[:, "width"] == 350).all()
+    assert np.allclose(df.loc[:, "prob_notumor"], 0.9525967836380005)
+    assert np.allclose(df.loc[:, "prob_tumor"], 0.04740329459309578)
