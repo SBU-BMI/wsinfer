15a594de85173a95130bbb555806fff099fafc9b
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2022-12-25 16:17:30 -0500

    use image tag kaczmarj/wsinfer

--- scripts/build_docker_images.sh
+++ scripts/build_docker_images.sh
@@ -24,22 +24,22 @@ fi
 version=$1
 
 # Main image.
-docker build -t kaczmarj/patch-classification-pipeline:$version .
+docker build -t kaczmarj/wsinfer:$version .
 
 # TILs
-docker build -t kaczmarj/patch-classification-pipeline:$version-tils - < dockerfiles/tils.dockerfile
+docker build -t kaczmarj/wsinfer:$version-tils - < dockerfiles/tils.dockerfile
 
 # Tumor BRCA
-docker build -t kaczmarj/patch-classification-pipeline:$version-tumor-brca - < dockerfiles/tumor-brca.dockerfile
+docker build -t kaczmarj/wsinfer:$version-tumor-brca - < dockerfiles/tumor-brca.dockerfile
 
 # Tumor LUAD
-docker build -t kaczmarj/patch-classification-pipeline:$version-tumor-luad - < dockerfiles/tumor-luad.dockerfile
+docker build -t kaczmarj/wsinfer:$version-tumor-luad - < dockerfiles/tumor-luad.dockerfile
 
 # Tumor PAAD
-docker build -t kaczmarj/patch-classification-pipeline:$version-tumor-paad - < dockerfiles/tumor-paad.dockerfile
+docker build -t kaczmarj/wsinfer:$version-tumor-paad - < dockerfiles/tumor-paad.dockerfile
 
 # Tumor PRAD
-docker build -t kaczmarj/patch-classification-pipeline:$version-tumor-prad - < dockerfiles/tumor-prad.dockerfile
+docker build -t kaczmarj/wsinfer:$version-tumor-prad - < dockerfiles/tumor-prad.dockerfile
 
 # Push images.
 push_images="${2:-0}"
@@ -47,10 +47,10 @@ if [ $push_images -eq 0 ]; then
     echo "Not pushing images. Pass a 1 to the script to push images."
 else
     echo "Pushing images."
-    docker push kaczmarj/patch-classification-pipeline:$version
-    docker push kaczmarj/patch-classification-pipeline:$version-tils
-    docker push kaczmarj/patch-classification-pipeline:$version-tumor-brca
-    docker push kaczmarj/patch-classification-pipeline:$version-tumor-luad
-    docker push kaczmarj/patch-classification-pipeline:$version-tumor-paad
-    docker push kaczmarj/patch-classification-pipeline:$version-tumor-prad
+    docker push kaczmarj/wsinfer:$version
+    docker push kaczmarj/wsinfer:$version-tils
+    docker push kaczmarj/wsinfer:$version-tumor-brca
+    docker push kaczmarj/wsinfer:$version-tumor-luad
+    docker push kaczmarj/wsinfer:$version-tumor-paad
+    docker push kaczmarj/wsinfer:$version-tumor-prad
 fi
