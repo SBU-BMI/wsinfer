f23930be5f2413f522409744697858472cfe6e21
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-01-01 22:36:08 -0500

    expose public api in _modellib

--- wsinfer/__init__.py
+++ wsinfer/__init__.py
@@ -1,3 +1,13 @@
+"""WSInfer is a toolkit for fast patch-based inference on whole slide images."""
 
 from . import _version
-__version__ = _version.get_versions()['version']
+from ._modellib.models import get_model_weights  # noqa
+from ._modellib.models import list_all_models_and_weights  # noqa
+from ._modellib.models import register_model_weights  # noqa
+from ._modellib.run_inference import run_inference  # noqa
+from ._modellib.run_inference import WholeSlideImagePatches  # noqa
+from ._modellib.transforms import PatchClassification  # noqa
+
+__version__ = _version.get_versions()["version"]
+
+del _version
