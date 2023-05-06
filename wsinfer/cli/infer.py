d2b79afa5b945e51e6bc452a11e4088af08ca3bb
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-05-06 16:48:54 -0400

    [ENH] adds ability to run inference inside ROI (#114)
    * add feature to filter patches by ROI
    
    This will run model inference only within ROIs.
    
    * test if geojson is valid + rm unused var
    
    * use --roi-dir to provide directory of ROI files
    
    * check that --roi-dir is a directory and not a file
    
    * rm extra space when printing roi path
    
    * add tmp debug prints
    
    * fix: return original coords + rm debug prints
    
    * missed one tmp print
    
    * ignore types for geojson and shapely
    
    Also remove geojson from dev dependencies because it is already a
    core dependency.

--- wsinfer/cli/infer.py
+++ wsinfer/cli/infer.py
@@ -247,6 +247,19 @@ def _get_info_for_save(weights: models.Weights):
     help="JIT-compile the model for potential speedups.",
 )
 @click.option(
+    "--roi-dir",
+    type=click.Path(
+        exists=True, dir_okay=True, file_okay=False, path_type=Path, resolve_path=True
+    ),
+    help=(
+        "Directory of regions of interest (ROIs) encoded as GeoJSON files. Files must"
+        " be named with SLIDE_ID.json, where SLIDE_ID is the name of the whole slide"
+        " image without its suffix. The ROI will be used only if the file exists. If"
+        " an ROI file is not found, then all patches within tissue will be used."
+    ),
+    default=None,
+)
+@click.option(
     "--dense-grid/--no-dense-grid",
     default=False,
     show_default=True,
@@ -264,6 +277,7 @@ def run(
     batch_size: int,
     num_workers: int = 0,
     speedup: bool = False,
+    roi_dir: typing.Optional[PathType] = None,
     dense_grid: bool = False,
 ):
     """Run model inference on a directory of whole slide images.
@@ -344,6 +358,7 @@ def run(
         batch_size=batch_size,
         num_workers=num_workers,
         speedup=speedup,
+        roi_dir=roi_dir,
     )
 
     if failed_patching:
