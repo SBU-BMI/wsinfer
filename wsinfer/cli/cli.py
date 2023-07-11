ae38ae6a9009dd40f398c245dae4504a16b79d92
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-07-11 15:49:19 -0400

    add logging and backend selection to cli

--- wsinfer/cli/cli.py
+++ wsinfer/cli/cli.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+import logging
+from typing import Literal
+
 import click
 
 from .convert_csv_to_geojson import togeojson
@@ -5,14 +10,37 @@ from .convert_csv_to_sbubmi import tosbu
 from .infer import run
 from .patch import patch
 
+from ..wsi import set_backend
+
+_logging_levels = ["debug", "info", "warning", "error", "critical"]
+
 
 # We use invoke_without_command=True so that 'wsinfer' on its own can be used for
 # inference on slides.
 @click.group()
+@click.option(
+    "--backend",
+    default=None,
+    help="Backend for loading whole slide images.",
+    type=click.Choice(["openslide", "tiffslide"]),
+)
+@click.option(
+    "--log-level",
+    default="info",
+    type=click.Choice(_logging_levels),
+    help="Set the loudness of logging.",
+)
 @click.version_option()
-def cli():
+def cli(backend: Literal["openslide"] | Literal["tiffslide"] | None, log_level: str):
     """Run patch-level classification inference on whole slide images."""
-    pass
+
+    # Configure logger.
+    levels = {level: getattr(logging, level.upper()) for level in _logging_levels}
+    level = levels[log_level]
+    logging.basicConfig(level=level)
+
+    if backend is not None:
+        set_backend(backend)
 
 
 cli.add_command(run)
