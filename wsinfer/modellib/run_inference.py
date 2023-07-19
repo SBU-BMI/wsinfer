5ce028531803d0b873a3da93c41b15db4bbca7bf
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-07-19 12:53:48 -0400

    Remove model-specific dockerfiles + Remove ROI functionality (#155)
    * remove model-specific docker images
    
    * remove WSINFER_ env var prefix in wsinfer run
    
    * remove roi-dir functionality
    
    * do not require geojson on installation

--- wsinfer/modellib/run_inference.py
+++ wsinfer/modellib/run_inference.py
@@ -8,11 +8,9 @@ from __future__ import annotations
 
 import typing
 from pathlib import Path
-from typing import List
-from typing import Optional
-from typing import Tuple
 
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
 import torch
 import tqdm
@@ -33,8 +31,7 @@ def run_inference(
     batch_size: int = 32,
     num_workers: int = 0,
     speedup: bool = False,
-    roi_dir: Optional[str | Path] = None,
-) -> Tuple[List[str], List[str]]:
+) -> tuple[list[str], list[str]]:
     """Run model inference on a directory of whole slide images and save results to CSV.
 
     This assumes the patching has already been done and the results are stored in
@@ -113,13 +110,7 @@ def run_inference(
     transform = make_compose_from_transform_config(model_info.config.transform)
 
     failed_patching = [p.stem for p in patch_paths if not p.exists()]
-    failed_inference: List[str] = []
-
-    # Get paths to ROI geojson files.
-    if roi_dir is not None:
-        roi_paths = [Path(roi_dir) / p.with_suffix(".json").name for p in wsi_paths]
-    else:
-        roi_paths = None
+    failed_inference: list[str] = []
 
     # results_for_all_slides: typing.List[pd.DataFrame] = []
     for i, (wsi_path, patch_path) in enumerate(zip(wsi_paths, patch_paths)):
@@ -138,16 +129,6 @@ def run_inference(
             print(f"Skipping because patch file not found: {patch_path}")
             continue
 
-        roi_path = None
-        if roi_paths is not None:
-            roi_path = roi_paths[i]
-            # We grab all potential names of ROI paths, but we do not require all of
-            # them to exist. We only use those that exist.
-            if not roi_path.exists():
-                roi_path = None
-            else:
-                print(f" ROI path: {roi_path}")
-
         try:
             dset = WholeSlideImagePatches(
                 wsi_path=wsi_path,
@@ -155,7 +136,6 @@ def run_inference(
                 um_px=model_info.config.spacing_um_px,
                 patch_size=model_info.config.patch_size_pixels,
                 transform=transform,
-                roi_path=roi_path,
             )
         except Exception:
             failed_inference.append(wsi_dir.stem)
@@ -176,8 +156,8 @@ def run_inference(
 
         # Store the coordinates and model probabiltiies of each patch in this slide.
         # This lets us know where the probabiltiies map to in the slide.
-        slide_coords: List[np.ndarray] = []
-        slide_probs: List[np.ndarray] = []
+        slide_coords: list[npt.NDArray[np.int_]] = []
+        slide_probs: list[npt.NDArray[np.float_]] = []
         for batch_imgs, batch_coords in tqdm.tqdm(loader):
             assert batch_imgs.shape[0] == batch_coords.shape[0], "length mismatch"
             with torch.no_grad():
